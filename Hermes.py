f8ab343c53c1ff39db4c93c4db87602ad240e326
nightfire8199 <comstar8199@gmail.com>
2015-04-21 20:12:42 -0400

    Cleaned up code

--- Hermes.py
+++ Hermes.py
@@ -44,24 +44,10 @@ print "              |__/   \n"
 
 user = User()
 client = Client_Handler(user)
-
-print ""
-
-
-# cursor.execute('''
-# 	SELECT artist, album, title FROM tracks
-# 	''')
-# user1 = cursor.fetchone() #retrieve the first row
-# print(user1[0]) #Print the first column retrieved(user's name)
-# all_rows = cursor.fetchall()
-# for row in all_rows:
-#     # row[0] returns the first column in the query (name).
-#     print row[0].encode("utf-8"), row[1].encode("utf-8"), row[2].encode("utf-8")
-
-### Allow User Search
-
 player = Player()
 player.client = client
+
+print ""
 
 func_dict = {
 	'play' : play,
@@ -75,6 +61,17 @@ func_dict = {
 	'clear': clear_queue,
 	'sync' : sync
 }
+
+def intersect(res, inp):
+	if(len(res) == 0):
+		for row in inp:
+	    		 res.add(row)
+	else:
+		temp = set()
+		for row in inp:
+	    		 temp.add(row)
+		res = res.intersection(temp)
+	return res
 
 while(True):
 	USI = raw_input("$> ")
@@ -97,44 +94,21 @@ while(True):
 		for word in USI.split():
 			user.cursor.execute("SELECT DISTINCT(artist) FROM tracks WHERE artist LIKE ? OR artist LIKE ? ORDER BY artist", (word+'%', '% '+word+'%',))
 			all_rows = user.cursor.fetchall()
-			if(len(Art_res) == 0):
-				for row in all_rows:
-			    		 Art_res.add(row[0].encode("utf-8"))
-			else:
-				temp = set()
-				for row in all_rows:
-			    		 temp.add(row[0].encode("utf-8"))
-				Art_res = Art_res.intersection(temp)
-
+			Art_res = intersect(Art_res, all_rows)
 			
 			user.cursor.execute("SELECT DISTINCT(album) FROM tracks WHERE album LIKE ? OR album LIKE ? ORDER BY album", (word+'%', '% '+word+'%',))
 			all_rows = user.cursor.fetchall()
-			if(len(Alb_res) == 0):
-				for row in all_rows:
-			    		 Alb_res.add(row[0].encode("utf-8"))
-			else:
-				temp = set()
-				for row in all_rows:
-			    		 temp.add(row[0].encode("utf-8"))
-				Alb_res = Alb_res.intersection(temp)
-
+			Alb_res = intersect(Alb_res, all_rows)
 			
 			user.cursor.execute("SELECT DISTINCT(id), artist, album, title FROM tracks WHERE title LIKE ? OR title LIKE ? ORDER BY artist, album", (word+'%', '% '+word+'%',))
 			all_rows = user.cursor.fetchall()
-			if(len(Tra_res) == 0):
-				for row in all_rows:
-			    		 Tra_res.add(row)
-			else:
-				temp = set()
-				for row in all_rows:
-			    		 temp.add(row)
-				Tra_res = Tra_res.intersection(temp)
+			Tra_res = intersect(Tra_res, all_rows)
 
 		print "\n...ARTISTS..............."
-		for artist in Art_res:
+		for [artist] in Art_res:
 			print artist
 		print "\n...ALBUMS..............."
-		for album in Alb_res:
+		for [album] in Alb_res:
 			print album
 		print "\n...TRACKS..............."
 		for [ident,artist,album,track] in Tra_res:
